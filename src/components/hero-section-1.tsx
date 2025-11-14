755c4ffb66d46a3e7a84cdd9b7a5e69a9c8b7411
dhaanisi <dhanishende2304@gmail.com>
2025-11-15 01:11:11 +0530

    logo-changed(bug)

--- src/components/hero-section-1.tsx
+++ src/components/hero-section-1.tsx
@@ -97,12 +97,12 @@ export function HeroSection() {
                 className="absolute inset-0 -z-20"
               >
                 <Image
-                  src="https://ik.imagekit.io/lrigu76hy/tailark/night-background.jpg?updatedAt=1745733451120"
-                  alt="background"
-                  className="absolute inset-x-0 top-56 -z-20 hidden lg:top-32 dark:block"
-                  width="3276"
-                  height="4095"
-                />
+                      src="/public/logo.jpg"
+                      alt="background logo"
+                      className="absolute inset-0 -z-20 opacity-[0.05] object-contain mx-auto"
+                      width={1800}
+                      height={1800}
+                    />
               </AnimatedGroup>
               <div
                 aria-hidden
